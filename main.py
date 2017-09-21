6a45dbe8b2b70c7c3355c74c223747325e41488d
Dena Shaw <dena.l.shaw@gmail.com>
2017-09-21 16:58:37 -0500

    first try

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+    <html>
+        <head>
+            <style>
+                form {
+                    background-color: #eee;
+                    padding: 20px;
+                    margin: 0 auto;
+                    width: 540px;
+                    font: 16px sans-serif;
+                    border-radius: 10px;
+                }
+                textarea {
+                    margin: 10px 0;
+                    width: 540px;
+                    height: 120px;
+                }
+            </style>
+        </head>
+        <body>
+        <!-- create your form here -->
+            <form action="/encode" id="form" method="post">
+                    Rotate by:<input type ="text" name="rot" value="0"/><br><br>
+                    <input type="text" name="text"/><textarea name="text" form="form"/><br>
+                <input type="submit" value="Encode"/>
+            </form>
+        </body>
+    </html>
+"""
+
+@app.route("/")
+def index():
+    return form
+
+app.run()
